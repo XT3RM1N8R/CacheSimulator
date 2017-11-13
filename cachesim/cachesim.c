4a2b7a28289738c1d5411b4f57c2b8e0d49615be
Darien Sokolov <darien.sokolov@yahoo.com>
2017-11-13 04:55:08 -0600

    Revert "Revert "Initial upload""
    This reverts commit 1d3f36ff7b12c73a1cd6089c4d5876530d0b5345.

---
+++ cachesim/cachesim.c
@@ -0,0 +1,113 @@
+/*
+ * CS3375 Computer Architecture
+ * Course Project
+ * Cache Simulator Design and Development
+ * FALL 2017
+ */
+
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <math.h>
+#include "cachesim.h"
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        printf("Usage: %s <direct> <trace file name>\n", argv[0]);
+        return 1;
+    }
+
+    printf("BLOCK SIZE = %d Bytes\n", BLOCK_SIZE);
+    printf("%d-WAY\n", WAY_SIZE);
+    printf("CACHE SIZE = %d Bytes\n", CACHE_SIZE);
+    printf("NUMBER OF BLOCKS = %d\n", NUM_BLOCKS);
+    printf("NUMBER OF SETS = %d\n", NUM_SETS);
+    printf("\n");
+
+    char* trace_file_name = argv[2];
+    struct direct_mapped_cache d_cache;
+    char mem_request[20];
+    uint64_t address;
+    FILE *fp;
+
+    for (int i=0; i<NUM_BLOCKS; i++) { /* Initialization */
+        d_cache.valid_field[i] = 0;
+        d_cache.dirty_field[i] = 0;
+        d_cache.tag_field[i] = 0;
+    }
+    d_cache.hits = 0;
+    d_cache.misses = 0;
+
+    fp = fopen(trace_file_name, "r"); /* Opening the memory trace file */
+
+    if (strncmp(argv[1], "direct", 6)==0) { /* Simulating direct-mapped cache */
+        while (fgets(mem_request, 20, fp)!= NULL) { /* Read the memory request address and access the cache */
+            address = convert_address(mem_request);
+            direct_mapped_cache_access(&d_cache, address);
+        }
+        printf("\n==================================\n"); /* Print out the results */
+        printf("Cache type:    Direct-Mapped Cache\n");
+        printf("==================================\n");
+        printf("Cache Hits:    %d\n", d_cache.hits);
+        printf("Cache Misses:  %d\n", d_cache.misses);
+        printf("\n");
+    }
+
+    fclose(fp);
+    return 0;
+}
+
+uint64_t convert_address(char memory_addr[]) { /* Converts the physical 32-bit address in the trace file to the "binary" */
+    uint64_t binary = 0;                       /* (a uint64 that can have bitwise operations on it) */
+    int i = 0;
+
+    while (memory_addr[i] != '\n') {
+        if (memory_addr[i] <= '9' && memory_addr[i] >= '0') {
+            binary = (binary*16) + (memory_addr[i] - '0');
+        } else {
+            if(memory_addr[i] == 'a' || memory_addr[i] == 'A') {
+                binary = (binary*16) + 10;
+            }
+            if(memory_addr[i] == 'b' || memory_addr[i] == 'B') {
+                binary = (binary*16) + 11;
+            }
+            if(memory_addr[i] == 'c' || memory_addr[i] == 'C') {
+                binary = (binary*16) + 12;
+            }
+            if(memory_addr[i] == 'd' || memory_addr[i] == 'D') {
+                binary = (binary*16) + 13;
+            }
+            if(memory_addr[i] == 'e' || memory_addr[i] == 'E') {
+                binary = (binary*16) + 14;
+            }
+            if(memory_addr[i] == 'f' || memory_addr[i] == 'F') {
+                binary = (binary*16) + 15;
+            }
+        }
+        i++;
+    }
+    printf("%s converted to %llu\n", memory_addr, binary);
+    return binary;
+}
+
+void direct_mapped_cache_access(struct direct_mapped_cache *cache, uint64_t address) {
+    uint64_t block_addr = address >> (unsigned)log2(BLOCK_SIZE);
+    uint64_t index = block_addr % NUM_BLOCKS;
+    uint64_t tag = block_addr >> (unsigned)log2(NUM_BLOCKS);
+
+    printf("Memory address: %llu, Block address: %llu, Index: %llu, Tag: %llu ", address, block_addr, index, tag);
+
+    if (cache->valid_field[index] && cache->tag_field[index] == tag) { /* Cache hit */
+        cache->hits += 1;
+        printf("Hit!\n");
+    } else { /* Cache miss */
+        cache->misses += 1;
+        printf("Miss!\n");
+        if (cache->valid_field[index] && cache->dirty_field[index]) { /* Write the cache block back to memory */
+        }
+        cache->tag_field[index] = tag;
+        cache->valid_field[index] = 1;
+        cache->dirty_field[index] = 0;
+    }
+}
+
